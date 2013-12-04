de2301ed0573bfe4b453bf613992c1bb992e6a78
elseym <elseym@me.com>
2013-12-04 17:27:40 +0100

    fix whereami

--- whereami.rb
+++ whereami.rb
@@ -2,10 +2,8 @@ require 'formula'
 
 class Whereami < Formula
   homepage 'https://github.com/robmathers/WhereAmI'
-  url 'https://github.com/robmathers/WhereAmI/releases/download/v1.02/whereami-1.02.zip'
+  url 'https://github.com/robmathers/WhereAmI/archive/v1.02.tar.gz'
   sha1 '77d268416ae0749b8f5251397b78f1bebe65f890'
-
-  head 'https://github.com/robmathers/WhereAmI.git'
 
   def install
     system "xcodebuild", "-project", "WhereAmI.xcodeproj",
